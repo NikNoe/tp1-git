73dde5822521707af8e94762a44a71236509f602
Ian Kenny <ian-kenny.ngoumja@esme.fr>
2023-10-03 15:37:20 +0200

    suppression du dernier commit via git revert

--- main.c
+++ main.c
@@ -3,7 +3,5 @@ int main() {
    // printf() displays the string inside quotation
    printf("Welcome in cours de git");
    //ajout d'un commentaire   
-   //ajout d'une ligne sans faire de commit
-   //repertoire de travail et commit index
    return 0;
 }
